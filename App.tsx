4f7186a4b06794e7f05d9c9775f34bb86383978b
Dano <danocolombo@gmail.com>
2023-12-03 16:07:13 -0500

    end of Devember - Day 1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // create an array 1-24
+    const days = [...Array(24)].map((val, index) => index + 1);
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={days}
+                contentContainerStyle={styles.content}
+                columnWrapperStyle={styles.column}
+                numColumns={2}
+                renderItem={({ item }) => <DayListItem day={item} />}
+            />
+            <StatusBar style='auto' />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    content: {
+        gap: 10,
+        padding: 10,
+    },
+    column: {
+        gap: 10,
+    },
 });
---
+++ src/components/core/DayListItem.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+type DayListItem = {
+    day: number;
+};
+
+const DayListItem = ({ day }: DayListItem) => {
+    return (
+        <View style={styles.box}>
+            <Text style={styles.text}>{day}</Text>
+        </View>
+    );
+};
+
+export default DayListItem;
+
+const styles = StyleSheet.create({
+    box: {
+        backgroundColor: '#f9ede3',
+        flex: 1,
+        aspectRatio: 1,
+        // width: 100,
+        // height: 100,
+        // flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: '#9b4521',
+        borderRadius: 20,
+    },
+    text: {
+        color: '#9b4521',
+        fontSize: 70,
+    },
+});
